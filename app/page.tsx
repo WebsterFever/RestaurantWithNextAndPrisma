f396c26b89d7f8e9b3fc7414be91aa621dd268c4
WebsterFever <webstersevenlanguages@gmail.com>
2024-05-06 11:28:29 -0400

    feat: add initial header component

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,9 @@
-const name = "";
+import Header from "./_components/ui/header"
 
 const Home = () => {
   return (
 
-    <div className="bg-red-500 p-5">
-       <h1>Hello Tailwindd</h1>
-    </div>
+    <Header/>
 
   )
 }
