44d4dfe30ac751a50dcb4097b1795321f51107e7
WebsterFever <webstersevenlanguages@gmail.com>
2024-05-08 10:06:43 -0400

    feat:add promo banner to homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import CategoryList from "./_components/ui/cotegory-list";
 import Header from "./_components/ui/header"
 import Search from "./_components/ui/search";
+import Image from "next/image";
 
 const Home = () => {
   return (
@@ -14,7 +15,16 @@ const Home = () => {
     <div className="px-5 pt-6">
     <CategoryList/>
     </div>
+     
     
+    <div className="px-5 pt-6">
+    <Image src="/promo-banner-01.png" alt="fsw foods" 
+    height={0} 
+    width={0}
+    className="h-auto w-full object-contain"
+    sizes="100vw"/>
+    </div>
+
      
     </>
     
